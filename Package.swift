200e880f49640ef8bb6314b2715a00295875ef31
Patrick Maltagliati <patmalt@gmail.com>
2020-12-07 15:54:06 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,6 @@ let package = Package(
         .package(url: "https://github.com/soto-project/soto.git", from: "5.0.0-rc.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftAwsLambdaStarter",
             dependencies: [
